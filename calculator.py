25c5d6d86edeab9732302879dd3f729cc2a7d4f7
Lola <lapinelobo@gmail.com>
2019-06-20 15:34:22 +0000

    initial commit

---
+++ calculator.py
@@ -0,0 +1,17 @@
+#print("Hello Calculator")
+
+ravioli = 0
+spaggeti = 0
+macNchez = 0
+
+print("Welcome to the pasta quiz!")
+
+Inpt = input("You kidnap your worst enemey. Do you (a) sell their organs (b) make them work for you or (c) C o n s u m e them")
+if Inpt == "a":
+    
+elif Inpt == "b":
+    
+elif Inpt == "c":
+    
+
+#then at the end we need if/else statements to count which has the most points
